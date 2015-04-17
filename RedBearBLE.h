dd7268c0afde72685439405179518591914ecee1
Kelvin Luck <git@kelvinluck.com>
2015-04-17 11:20:48 +0100

    Basic implementation of the first necessary parts for BLE communication

--- RedBearBLE.h
+++ RedBearBLE.h
@@ -1,5 +1,9 @@
 #import "RCTBridgeModule.h"
+#import "RCTLog.h"
+#import "BLE.h"
 
-@interface RedBearBLE : NSObject <RCTBridgeModule>
+@interface RedBearBLE : NSObject <BLEDelegate, RCTBridgeModule>
+
+@property (strong, nonatomic) BLE *ble;
 
 @end
--- RedBearBLE.m
+++ RedBearBLE.m
@@ -1,10 +1,81 @@
 #import "RedBearBLE.h"
 
+@interface RedBearBLE()
+
+-(void) bleConnect;
+-(void) connectionTimer:(NSTimer *)timer;
+
+@end
+
 @implementation RedBearBLE
 
-- (void)test
+RCT_EXPORT_MODULE();
+
+@synthesize ble;
+
+- (instancetype)init
 {
-  RCT_EXPORT();
+    if ((self = [super init])) {
+        self.ble = [[BLE alloc] init];
+        [self.ble controlSetup];
+        self.ble.delegate = self;
+    }
+    return self;
+}
+
+RCT_EXPORT_METHOD(connect)
+{
+    [self bleConnect];
+}
+
+// BLE
+
+
+-(void) bleDidConnect {
+    RCTLogInfo(@"Connected");
+};
+-(void) bleDidDisconnect {
+    RCTLogInfo(@"Disconnected");
+    
+};
+-(void) bleDidUpdateRSSI:(NSNumber *) rssi {
+    RCTLogInfo(@"bleDidUpdateRSSI %d", rssi.intValue);
+};
+-(void) bleDidReceiveData:(unsigned char *) data length:(int) length {
+    
+};
+
+- (void)bleConnect
+{
+    if (ble.activePeripheral)
+        if(ble.activePeripheral.state == CBPeripheralStateConnected)
+        {
+            [[ble CM] cancelPeripheralConnection:[ble activePeripheral]];
+            RCTLogInfo(@"Thing connected to other periperial");
+            return;
+        }
+    
+    if (ble.peripherals)
+        ble.peripherals = nil;
+    
+    [ble findBLEPeripherals:2];
+    RCTLogInfo(@"Searching for thing");
+    
+    [NSTimer scheduledTimerWithTimeInterval:(float)2.0 target:self selector:@selector(connectionTimer:) userInfo:nil repeats:NO];
+}
+
+-(void) connectionTimer:(NSTimer *)timer
+{
+    
+    if (ble.peripherals.count > 0)
+    {
+        [ble connectPeripheral:[ble.peripherals objectAtIndex:0]];
+        RCTLogInfo(@"Connecting");
+    }
+    else
+    {
+        RCTLogInfo(@"Connection timeout");
+    }
 }
 
 @end
